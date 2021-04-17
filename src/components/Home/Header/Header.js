46ab8092e922f4413a73f9455cc40936d8e09dbd
fardinkhan69 <fardinofficial69@gmail.com>
2021-04-17 16:53:25 +0600

    done

--- src/components/Home/Header/Header.js
+++ src/components/Home/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
       <div class="container" data-aos="zoom-out" data-aos-delay="100">
         <div className="row">
           <div className="col-md-9">
-            <h1 className="text-left">Welcome to <br/> <span>Dhakaiya Salon</span></h1>
+            <h1 className="text-left">Welcome to <br/> <span>Dhakaiya Barberss</span></h1>
             <h2 className="text-left py-5">We are team of talented Barbers. And Our Goal is to Make you look your best <br />Now it's upon you that You wanted to be at the best stage of yours or not </h2>
             <div class="d-flex">
               <a href="#about" class="btn btn-brand text-white">Get A Hair Cut</a>
